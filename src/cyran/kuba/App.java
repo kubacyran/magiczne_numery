d7460c53023d7f914de4f2bdb4a8e3c8411f528c
gupikuba <gupikuba@gmail.com>
2020-02-23 12:16:58 +0100

    troche lepiej

--- src/cyran/kuba/App.java
+++ src/cyran/kuba/App.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args)throws IOException {
         FileDifferentiator fd = new FileDifferentiator();
-        fd.differentFile("koles.jpg");
+        fd.differentFile("koles.txt");
 
     }
 }
--- src/cyran/kuba/TxtTester.java
+++ src/cyran/kuba/TxtTester.java
@@ -4,7 +4,7 @@ public class TxtTester implements FileTester {
     @Override
     public boolean test(byte[] data) {
         for(byte b : data){
-            if((int)b > 127)
+            if((int)b > 127 || (int)b<0)
                 return false;
         }
         return true;
